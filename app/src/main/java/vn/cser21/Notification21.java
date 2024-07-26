54ade07012edb2a2c80e709661c30a303b8a6e8e
NguyenTuanEZS <nguyentuan@ezs.vn>
2024-07-26 15:23:38 +0700

    first commit

---
+++ app/src/main/java/vn/cser21/Notification21.java
@@ -0,0 +1,8 @@
+package tinhkhoedep.ezs;
+
+
+public  class Notification21 {
+    public String title;
+    public String body;
+    public String sound;
+}
---
+++ app/src/main/java/vn/cser21/incoming/EndCallEvent.java
@@ -0,0 +1,3 @@
+package tinhkhoedep.ezs.incoming;
+
+public class EndCallEvent { }
